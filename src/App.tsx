0a52d84fd346bfd779b56e737ebade2da4bcde21
volha <o.bukhal@gmail.com>
2022-06-01 16:50:53 +0300

    init: start calculator with install eslint&pritter

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import Layout from "components/Layout";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Start react calculator task</p>
+        <Layout />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Layout = () => {
+  return <div>Layout component</div>;
+};
+
+export default Layout;
